122c097fcf9feaea05dc7dce050c20b33a20e1b7
guddu <kguddu.1011@gmail.com>
2022-04-10 13:02:04 +0530

    code adde

---
+++ index.js
@@ -0,0 +1,19 @@
+// swap two nubers using third varaible;
+
+
+
+
+let a=10;
+let b=15;
+
+[a,b]=[b,a];
+
+console.log(`the value of a is ${a} and the value of b is ${b}.`);
+
+
+// .........ANSWER.........
+
+// The value of a is 15;
+//The value of b is 10;
+
+
